6e18af4758484c8d8af83d8a65c1f1bedb1f0f09
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-06-01 16:30:47 +0000

    fix game

--- scripts/tests/game.js
+++ scripts/tests/game.js
@@ -1,32 +1,33 @@
 let game = {
+    currentGame: [],
+    playerMoves: [],
     score: 0,
-    currentGame:[],
-    playerMoves:[],
-    choices:["button1","button2","button3","button4"],
-}
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
 function newGame() {
-    game.score = 0;
-    game.playerMoves = [];
     game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
     showScore();
     addTurn();
 }
 
-function showScore(){
-    document.getElementById("score").innerText = game.score;
-
-}
-function addTurn(){
+function addTurn() {
     game.playerMoves = [];
-    game.currentGame.push(game.choices[(Math.floor(Math.random()* 4))]);
-    //showTurns();
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
 }
 
-function lightsOn(circ){
-    document.getElementById(circ).classList.add("light");
-    setTimeout(()=>{
-        document.getElementById(circ).classList.remove("light");
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add(circ + "light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove(circ + "light");
     }, 400);
 }
-module.exports={game, newGame, showScore, addTurn, lightsOn};
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
